5665f9f3d6a26744ecc717dc6ec453efc10e9c32
KyungMoon Lee <leekm8474@gmail.com>
2017-06-27 20:36:21 +0900

    add gitignore and scalar summary

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ flags.DEFINE_integer("iterations",450000,"Number of iterations to train")
 flags.DEFINE_integer("batch_size",100,"Number of batch size in each iteration")
 flags.DEFINE_float("starting_learning_rate",0.01,"starting learning rate")
 flags.DEFINE_string("train_models","./train_models","path to save checkpoints")
+flags.DEFINE_string("log_dir","./logs","path to log summaries")
 FLAGS = flags.FLAGS
 MOMENTUM = 0.9#0.0005
 LEARNING_RATE_DECAY = 0.1
@@ -25,13 +26,20 @@ class PretrainedAlexNet(object):
 		self.is_test = _is_test
 		self.x = images
 		self.keep_prob = _keep_prob
+		self.conv1 = None
+		self.conv2 = None
+		self.conv3 = None
+		self.conv4 = None
+		self.conv5 = None
+		self.fc6 = None
+		self.fc7 = None
 		self.fc8 = None
 		self.pre_trained_model = np.load(open(os.getcwd() + "/dataset/bvlc_alexnet.npy", "rb"), encoding="latin1").item()
 		self.create()
 
 
 	def create(self):
-		conv1 = tf.layers.conv2d(
+		self.conv1 = tf.layers.conv2d(
 			inputs=self.x,
 			filters=96,
 			strides=4,
@@ -41,11 +49,12 @@ class PretrainedAlexNet(object):
 			bias_initializer=tf.constant_initializer(self.pre_trained_model["conv1"][1]),
 			padding="VALID",
 			activation=tf.nn.relu, name='conv1')
-		pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], padding="VALID", strides=2, name='pool1')
-		norm1 = tf.nn.lrn(pool1, depth_radius=2, bias=1.0, alpha=2.0 / 100000.0, beta=0.75,
+		norm1 = tf.nn.lrn(self.conv1, depth_radius=2, bias=1.0, alpha=2.0 / 100000.0, beta=0.75,
 						  name='norm1')
+		pool1 = tf.layers.max_pooling2d(inputs=norm1, pool_size=[3, 3], padding="VALID", strides=2, name='pool1')
 
-		conv2 = tf.layers.conv2d(
+
+		self.conv2 = tf.layers.conv2d(
 			inputs=pool1,
 			filters=256,
 			strides=1,
@@ -55,11 +64,11 @@ class PretrainedAlexNet(object):
 			bias_initializer=tf.constant_initializer(self.pre_trained_model["conv2"][1]),
 			padding="same",
 			activation=tf.nn.relu, name='conv2')
-		pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[3, 3], padding="valid", strides=2, name='pool2')
-		norm2 = tf.nn.lrn(pool2, depth_radius=2, bias=1.0, alpha=2.0 / 100000.0, beta=0.75, name='norm2')
+		norm2 = tf.nn.lrn(self.conv2, depth_radius=2, bias=1.0, alpha=2.0 / 100000.0, beta=0.75, name='norm2')
+		pool2 = tf.layers.max_pooling2d(inputs=norm2, pool_size=[3, 3], padding="valid", strides=2, name='pool2')
 
-		conv3 = tf.layers.conv2d(
-			inputs=norm2,
+		self.conv3 = tf.layers.conv2d(
+			inputs=pool2,
 			filters=384,
 			strides=1,
 			kernel_size=[3, 3],
@@ -68,8 +77,8 @@ class PretrainedAlexNet(object):
 			bias_initializer=tf.constant_initializer(self.pre_trained_model["conv3"][1]),
 			padding="SAME",
 			activation=tf.nn.relu, name='conv3')
-		conv4 = tf.layers.conv2d(
-			inputs=conv3,
+		self.conv4 = tf.layers.conv2d(
+			inputs=self.conv3,
 			filters=384,
 			strides=1,
 			kernel_size=[3, 3],
@@ -78,8 +87,8 @@ class PretrainedAlexNet(object):
 			bias_initializer=tf.constant_initializer(self.pre_trained_model["conv4"][1]),
 			padding="SAME",
 			activation=tf.nn.relu, name='conv4')
-		conv5 = tf.layers.conv2d(
-			inputs=conv4,
+		self.conv5 = tf.layers.conv2d(
+			inputs=self.conv4,
 			filters=256,
 			strides=1,
 			kernel_size=[3, 3],
@@ -88,23 +97,23 @@ class PretrainedAlexNet(object):
 			bias_initializer=tf.constant_initializer(self.pre_trained_model["conv5"][1]),
 			padding="SAME",
 			activation=tf.nn.relu, name='conv5')
-		pool5 = tf.layers.max_pooling2d(inputs=conv5, pool_size=[3, 3], padding="VALID", strides=2, name='pool5')
+		pool5 = tf.layers.max_pooling2d(inputs=self.conv5, pool_size=[3, 3], padding="VALID", strides=2, name='pool5')
 
 		pool_fc_flat = tf.reshape(pool5, [-1, 6 * 6 * 256])
 
-		fc6 = tf.layers.dense(inputs=pool_fc_flat, units=4096,
+		self.fc6 = tf.layers.dense(inputs=pool_fc_flat, units=4096,
 							  kernel_initializer=tf.constant_initializer(self.pre_trained_model["fc6"][0]),
 							  kernel_regularizer=tf.contrib.layers.l2_regularizer(0.0005),
 							  bias_initializer=tf.constant_initializer(self.pre_trained_model["fc6"][1]),
 							  activation=tf.nn.relu, name='fc6')
-		fc6_drop = tf.nn.dropout(fc6, self.keep_prob)
+		fc6_drop = tf.nn.dropout(self.fc6, self.keep_prob)
 
-		fc7 = tf.layers.dense(inputs=fc6, units=4096,
+		self.fc7 = tf.layers.dense(inputs=fc6_drop, units=4096,
 							  kernel_initializer=tf.constant_initializer(self.pre_trained_model["fc7"][0]),
 							  kernel_regularizer=tf.contrib.layers.l2_regularizer(0.0005),
 							  bias_initializer=tf.constant_initializer(self.pre_trained_model["fc7"][1]),
 							  activation=tf.nn.relu, name='fc7')
-		fc7_drop = tf.nn.dropout(fc7, self.keep_prob)
+		fc7_drop = tf.nn.dropout(self.fc7, self.keep_prob)
 		self.fc8 = tf.layers.dense(inputs=fc7_drop, units=30,
 							  # kernel_initializer= tf.constant_initializer(pre_trained_model["fc8"][0]),
 							  # bias_initializer = tf.constant_initializer(pre_trained_model["fc8"][1]),
@@ -165,20 +174,44 @@ def train():
 		model = PretrainedAlexNet(images=x_image,_keep_prob=keep_prob)
 
 		global_step = tf.get_variable("global_step",initializer=0)
+		tf.summary.scalar('global_step', global_step)
+
 		lr = tf.train.exponential_decay(FLAGS.starting_learning_rate, global_step, NUM_EPOCHS_PER_LEARNING_RATE_DECAY,
 										LEARNING_RATE_DECAY, staircase=True)
-		regularization_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
+		tf.summary.scalar('learning_rate', lr)
 
+		regularization_loss = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
 		cross_entropy = (tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=model.fc8))+
-						 regularization_loss)
+						 sum(regularization_loss))
+		tf.summary.scalar('cross_entropy', cross_entropy)
 
 		train_step  = tf.train.MomentumOptimizer(lr, WEIGHT_DECAY_FACTOR).minimize(cross_entropy,global_step=global_step)
 
 		correct_prediction = tf.equal(tf.argmax(model.fc8, 1), tf.argmax(y_, 1))
 		accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+		tf.summary.scalar('train_accuracy', accuracy)
+
+		is_top1 = tf.equal(tf.nn.top_k(model.fc8, k=1)[1][:, 0], tf.cast(tf.argmax(y_, 1), "int32"))
+		is_top2 = tf.equal(tf.nn.top_k(model.fc8, k=2)[1][:, 1], tf.cast(tf.argmax(y_, 1), "int32"))
+		is_top3 = tf.equal(tf.nn.top_k(model.fc8, k=3)[1][:, 2], tf.cast(tf.argmax(y_, 1), "int32"))
+		is_in_top1 = is_top1
+		is_in_top2 = tf.logical_or(is_in_top1, is_top2)
+		is_in_top3 = tf.logical_or(is_in_top2, is_top3)
+
+		accuracy1 = tf.reduce_mean(tf.cast(is_in_top1, "float"))
+		accuracy1_summary = tf.summary.scalar('top1_accuracy', accuracy1)
+
+		accuracy2 = tf.reduce_mean(tf.cast(is_in_top2, "float"))
+		accuracy2_summary = tf.summary.scalar('top2_accuracy', accuracy2)
+
+		accuracy3 = tf.reduce_mean(tf.cast(is_in_top3, "float"))
+		accuracy3_summary = tf.summary.scalar('top3_accuracy', accuracy3)
+
+		merged = tf.summary.merge_all()
+		train_writer = tf.summary.FileWriter(FLAGS.log_dir + '/train', sess.graph)
+		test_writer = tf.summary.FileWriter(FLAGS.log_dir + '/test')
 
 		sess.run(tf.global_variables_initializer())
-
 
 		data_list = np.array_split(get_data(),TRAINING_SIZE)
 
@@ -186,28 +219,25 @@ def train():
 		ckpt = tf.train.get_checkpoint_state(FLAGS.train_models)
 		if ckpt and ckpt.model_checkpoint_path:
 			saver.restore(sess, ckpt.model_checkpoint_path)
-			print("restore done , global_step = %d" % sess.run(global_step) )#tf.assign()#tf.train.global_step(sess, global_step))
-
-		from datetime import datetime
-		start_time_str = datetime.now().strftime('%Y-%m-%d %H:%M')
+			print("restore done , global_step = %d" % sess.run(global_step) )
 
 		for i in range(sess.run(global_step)+1,FLAGS.iterations):
 			x, train_labels = features_from_data(data_list[i % TRAINING_SIZE])
 			train_step.run(feed_dict={x_: x, y_: train_labels, keep_prob: 0.5})
 
-			current_time_str = datetime.now().strftime('%Y-%m-%d %H:%M')
+			if i % 100 == 0: # if i % 100 == 0:
+				summary = sess.run(merged, feed_dict={x_: x, y_: train_labels, keep_prob: 1.0})
+				train_writer.add_summary(summary, i)
 
-			if i % 100 == 0 :
-				train_accuracy = accuracy.eval(feed_dict={
-					x_: x, y_: train_labels, keep_prob: 1.0})
-				print("step %d, training accuracy %g, current_:%s , start:%s , loss = %f , "
-					  "global_step = %d, learning_rate=%f " % (i, train_accuracy, current_time_str,
-					start_time_str, sess.run(cross_entropy, feed_dict={x_: x, y_: train_labels, keep_prob: 1.0})[1],
-					sess.run(global_step), sess.run(lr)))
-			if i % 5000 == 0:
+				test_data = get_data(is_test=True)
+				test_x, test_labels = features_from_data(test_data, is_test=True)
+				test_merged = tf.summary.merge([accuracy1_summary, accuracy2_summary, accuracy3_summary])
+				test_summary = sess.run(test_merged, feed_dict={x_: test_x, y_: test_labels, keep_prob: 1.0})
+				test_writer.add_summary(test_summary, i)
+
+			if i % 5000 == 0: #if i % 5000 == 0:
 				model_path = os.path.join(FLAGS.train_models,'model.ckpt')
 				saver.save(sess,model_path,global_step=global_step)
-				print("checkpoint saved")
 
 		model_path = os.path.join(FLAGS.train_models, 'model.ckpt')
 		saver.save(sess,model_path,global_step=global_step)
